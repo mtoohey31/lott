0bc830458d0da57c6dbe60d9d3c1089b01751422
Matthew Toohey <contact@mtoohey.com>
2025-01-18 15:49:07 -0500

    builtin Bool nonterminal

---
+++ Lott/DSL/Elab/Bool.lean
@@ -0,0 +1,28 @@
+import Lott.DSL.Elab
+
+namespace Lott.DSL.Elab
+
+open Lean
+open Lean.Elab
+open Lean.Parser
+
+declare_syntax_cat Lott.Symbol.Bool
+
+run_cmd setEnv <| aliasExt.addEntry (← getEnv) { canon := `Bool, alias := `b }
+
+@[Lott.Symbol.Bool_parser]
+def bool.b_parser : Parser := leadingNode `Lott.Symbol.Bool maxPrec <| identPrefix "b"
+
+@[lott_term_elab Lott.Symbol.Bool]
+def boolTermElab : TermElab
+  | _, .node _ `Lott.Symbol.Bool #[n@(.ident ..)] =>
+    Term.elabTerm n <| .some (.const `Bool [])
+  | _, _ => throwUnsupportedSyntax
+
+@[lott_tex_elab Lott.Symbol.Bool]
+def boolTexElab : TexElab
+  | _, Syntax.node _ `Lott.Symbol.Bool #[i@(.ident ..)] =>
+    return texEscape <| i.getId.toString (escape := false)
+  | _, _ => throwUnsupportedSyntax
+
+end Lott.DSL.Elab
