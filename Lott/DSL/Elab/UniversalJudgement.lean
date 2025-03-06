2008a72ac7d1c1058346312585014c0b6267dfc2
Matthew Toohey <contact@mtoohey.com>
2025-03-06 16:45:52 -0500

    allow type annotations in universal judgements

--- Lott/DSL/Elab/UniversalJudgement.lean
+++ Lott/DSL/Elab/UniversalJudgement.lean
@@ -4,17 +4,25 @@ import Lott.DSL.Parser.UniversalJudgement
 namespace Lott.DSL.Elab
 
 open Lean.Elab
+open Lean.Parser.Term
 
 @[lott_term_elab Lott.DSL.universalJudgement]
 private
 def universalJudgementTermElab : TermElab := fun _ stx => do
-  let `(Lott.Judgement| ∀ $i $[$binderPred?]?, $«judgement»:Lott.Judgement) := stx
+  let `(Lott.Judgement| ∀ $[$binders]* $[$type?]?, $«judgement»:Lott.Judgement) := stx
     | throwUnsupportedSyntax
-  let stx' ← if let some bp := binderPred? then
-      ``(∀ $i:ident, satisfies_binder_pred% $i $bp → [[$«judgement»:Lott.Judgement]])
-    else
-      ``(∀ $i:ident, [[$«judgement»:Lott.Judgement]])
+  let stx' ← ``(∀ $binders* $[$type?:typeSpec]?, [[$«judgement»:Lott.Judgement]])
+  Lean.Elab.Term.elabTerm stx' none
+
+@[lott_term_elab Lott.DSL.universalPredJudgement]
+private
+def universalPredJudgementTermElab : TermElab := fun _ stx => do
+  let stx' ← match stx with
+  | `(Lott.Judgement| ∀ $i:ident $bp:binderPred, $«judgement»:Lott.Judgement) =>
+    ``(∀ $i:ident, satisfies_binder_pred% $i $bp → [[$«judgement»:Lott.Judgement]])
+  | `(Lott.Judgement| ∀ $i:ident, $«judgement»:Lott.Judgement) =>
+    ``(∀ $i:ident, [[$«judgement»:Lott.Judgement]])
+  | _ => throwUnsupportedSyntax
   Lean.Elab.Term.elabTerm stx' none
 
 end Lott.DSL.Elab
-
--- Lott/DSL/Parser/UniversalJudgement.lean
+++ Lott/DSL/Parser/UniversalJudgement.lean
@@ -2,6 +2,10 @@ import Lott.DSL.Parser
 
 namespace Lott.DSL
 
-syntax (name := universalJudgement) "∀ " ident (binderPred)? ", " withPosition(Lott.Judgement) : Lott.Judgement
+open Lean.Parser.Term
+
+syntax (name := universalJudgement) "∀ " (ppSpace (binderIdent <|> bracketedBinder))+ optType ", " withPosition(Lott.Judgement) : Lott.Judgement
+
+syntax (name := universalPredJudgement) "∀ " ident binderPred ", " withPosition(Lott.Judgement) : Lott.Judgement
 
 end Lott.DSL
